c576e86be2570df2f6a77362283740d3b7ddbb4a
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-02-08 10:40:18 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //% color="#83de8a"
 namespace Blur {
     //% block
-    export function PixelSize (number: number, time: number) {
+    export function PixelSizeForMS (number: number, time: number) {
         let picturesprite: Sprite = sprites.create(img`
                 ................................................................................................................................................................
                 ................................................................................................................................................................
@@ -273,6 +273,7 @@ namespace Blur {
             }
             y = 0
             time
+            picturesprite.destroy ()
         }
     }
         //% block
