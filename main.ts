400fbd33078b726ca276dd851c92964508e21640
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-02-22 22:17:29 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -130,6 +130,7 @@ namespace Blur {
         let x = 0
         let zLayer = 0
         let isrendering = true
+        isrendering = true
         const buf = Buffer.create(120)
         const myRenderable = scene.createRenderable(zLayer, (image: Image, camera: scene.Camera) => {
         for (let x = 0; x < 160; x++) {
