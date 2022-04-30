cb6105219db503ff95893de3657b13d6061830af
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-04-29 23:10:29 -0400

    Restore '4/29/2022, 11:08:29 PM Update main.ts'

--- main.ts
+++ main.ts
@@ -130,7 +130,7 @@ namespace Blur {
         let x = 0
         let zLayer = 0
         let buf = Buffer.create(120)
-        let myRenderable = scene.createRenderable(zLayer, (image: Image) => {
+        let myRenderable = scene.createRenderable(zLayer, (image: Image, camera: scene.Camera) => {
         for (let x = 0; x < 160; x++) {
                 // Read the current screen content for modification
                 image.getRows(x, buf)
