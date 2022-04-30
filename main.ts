a148f1f992b845f1dbbf868d55153bff664c2c81
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-04-29 22:58:38 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -164,8 +164,7 @@ namespace Blur {
             y = 0                
         }}}})
         myRenderable.destroy()
-        pause(40)
-        setTimeout(() => sprite.destroy(), 20)
+        control.runInBackground (() => setTimeout(() => sprite.destroy(), 20))
     }
     //% block
     export function FadeOut () {
