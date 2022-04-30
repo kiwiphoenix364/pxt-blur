0640cce76f92712ec3d7d58efa8066fe11ccf4f4
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-04-30 15:52:55 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -16,17 +16,8 @@ namespace Blur {
                 for (let y = 0; y < 120; y++) {
                     var1 = (Math.round(x / size)) * size
                     var2 = (Math.round(y / size)) * size
-                    if (var1 <= 159 && var2 <= 119) {
                     buf[y] = image.getPixel(var1, var2)
-                    } else {
-                        if (var1 > 159 && var2 > 119) {
-                            buf[y] = image.getPixel(159, 119)
-                        } else if (var2 + (size) > 119) {
-                            buf[y] = image.getPixel(var1, 119)
-                        } else {
-                            buf[y] = image.getPixel(159, var2)
-                        }
-                    }
+                    
                 // Write the modified pixels back to the screen.
                 image.setRows(x, buf)
                 }             
