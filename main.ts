514f12787c2794484b178aac487794a4ecd1a3ac
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-04-30 17:12:10 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -13,8 +13,9 @@ namespace Blur {
                 // Read the current screen content for modification
                 // Now "buf" contains a color value for the current pixel row 
                 // (it's actually a vertical column onscreen) where it can be modified.) 
-            
+                var1 = (Math.round(x / size)) * size + (size / 2)
                 for (let y = 0; y < 120; y++) {
+                    var2 = (Math.round(y / size)) * size + (size / 2)
                     if (var2 - size <= 159 && var2 - size<= 119) {
                     buf[y] = image.getPixel(var1, var2)
                     } else {
@@ -26,11 +27,9 @@ namespace Blur {
                             buf[y] = image.getPixel(159, var2)
                         }
                     }
-                    var2 = (Math.round(y / size)) * size + (size / 2)
                 // Write the modified pixels back to the screen.
                 image.setRows(x, buf)
                 }
-        var1 = (Math.round(x / size)) * size + (size / 2)
         }})
         
     }
