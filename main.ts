4da45894bf017a8f7077bc60620c554dc98bb115
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-05-02 17:07:21 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -69,27 +69,26 @@ namespace Blur {
         let var1 = 0
         let var2 = 0
         let zLayer = 0
-        let number = 0
-        let numwidth2 = 0
-        let numheight2 = 0
+        let numwidth2 = size
+        let numheight2 = size
         let buf = Buffer.create(120)
         let r1 = [1]
         let myRenderable = scene.createRenderable(zLayer, (image: Image, camera: scene.Camera) => {
             for (let index4 = 0; index4 < 120 / numheight2; index4++) {
-                if (120 - y < number) {
+                if (120 - y < size) {
                     numheight2 = 120 - y
                 }
                 for (let index5 = 0; index5 < 160 / numwidth2; index5++) {
-                    if (160 - x < number) {
+                    if (160 - x < size) {
                         numwidth2 = 160 - x
                     }
                     image.fillRect(x, y, numwidth2, numheight2, image.getPixel(x + numwidth2 / 2, y + numheight2 / 2))
-                    x += number
-                    numwidth2 = number
+                    x += size
+                    numwidth2 = size
                 }
                 x = 0
-                y += number
-                numheight2 = number
+                y += size
+                numheight2 = size
             }
             y = 0
                     image.setRows(x, buf)
