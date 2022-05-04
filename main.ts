782b41dbd06c497bc1151002df9615f0d9807306
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-05-04 17:39:03 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -140,12 +140,12 @@ namespace Blur {
         let precalc = [0]
         let num1 = 0
         precalc = []
-        for (let index3 = 0; index3 < 119 + size / 2; index3++) {
-            precalc.push(Math.constrain((Math.round(index3 / size) * size) + size / 2, 0, 119))
+        for (let index3 = 0; index3 < 119; index3++) {
+            precalc.push(Math.constrain((Math.round(index3 / size)) * size + size / 2, 0, 119))
         }
         let variable = scene.createRenderable(zLayer, (image: Image, camera: scene.Camera) => {
             for (let index = 0; index < 159; index++) {
-                savedx = Math.constrain((Math.round(index / size) * size) + size / 2, 0, 159)
+                savedx = Math.constrain((Math.round(index / size)) * size + size / 2, 0, 159)
                 for (let index2 = 0; index2 < 119; index2++) {
                         if (!(precalc[index2] == index2)) {
                         buf[index2] = image.getPixel(savedx, precalc[index2])
