b08372b2ec23352ced500e773b535a3d95d1e12b
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-05-04 12:51:35 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -136,19 +136,21 @@ namespace Blur {
     export function NewSetBlurFilter(size: number) {
         let zLayer = 0
         let buf = Buffer.create(120)
-        let tempimg = sprites.create(screen.clone())
-        tempimg.setScale(1 / size)
+        
         let variable = scene.createRenderable(zLayer, (image: Image, camera: scene.Camera) => {
-                for (let index = 0; index < 159; index++) {
+                let tempimg = sprites.create(image.clone())
+                tempimg.setScale(1 / size)
+                    for (let index = 0; index < 159; index++) {
                     for (let index2 = 0; index2 < 119; index2++) {
                         buf[index2] = Math.round(tempimg.image.getPixel(index / size, index2 / size))
                     }
                 }
                 
+                tempimg.destroy()
                 }
             ) 
             setTimeout(() => variable.destroy(), 20)
-            tempimg.destroy()
+            
         }
             
 
