a660290e37b99ae29de651283e77247244be7f60
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-05-04 18:07:19 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -147,7 +147,7 @@ namespace Blur {
             for (let index = 0; index < 159; index++) {
                 savedx = Math.constrain((Math.round(index / size)) * size + size / 2, 0, 159)
                 for (let index2 = 0; index2 < 119; index2++) {
-                        buf[index2] = image.getPixel(savedx, precalc[index2])
+                        buf[index2] = image.getPixel(savedx - size / 2, precalc[index2] - size / 2)
                     }
                 image.setRows(index, buf)
                 }
