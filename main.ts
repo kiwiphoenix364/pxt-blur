81dedecfcf1febf2c88e8a7b58d8b42d6bc2af94
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-05-04 12:55:03 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -142,7 +142,8 @@ namespace Blur {
                 tempimg.setScale(0.5)
                     for (let index = 0; index < 159; index++) {
                     for (let index2 = 0; index2 < 119; index2++) {
-                        buf[index2] = tempimg.image.getPixel(index / 2, index2 / 2)
+                        //buf[index2] = tempimg.image.getPixel(index / 2, index2 / 2)
+                        buf[index2] = 15
                     }
                 }
                 
