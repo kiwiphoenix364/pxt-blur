e8cd12bb082e39b90172872ddf40eb8c27d41964
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-05-05 20:15:45 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -403,7 +403,6 @@ namespace Blur {
     //% picker=Mode
     //% block="Fade Out Over $mult ms, Use $mode To Fade"
     export function FadeOutOver(mult: number, mode: Mode) {
-        info.setScore(mode)
         if (mode == 0) {
         let img1 = image.screenImage().clone()
         let wait = ((66 + 2/3)* (mult / 1000))
